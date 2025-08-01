a0934a8ac76b999bb0bc9beae387cd97210e110c
mostafaadil <mostafaadil58@gmail.com>
2025-08-01 16:24:28 +0400

    enhance importing of Agent core

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const ReasoningAgent = require('./agents/reasoningAgent');
 const DeepSeekLLM = require('./tools/deepSeekLLM');
 const CrewManager = require('./crew/crewManager');
 
-const strategist = new ReasoningAgent("Eli", "Strategist", [DeepSeekLLM]);
 
-const crew = new CrewManager([strategist]);
-crew.runMission("Create a 3-step market entry strategy for an AI product in the EU.");
+
+
+module.exports={ReasoningAgent,DeepSeekLLM,CrewManager}
